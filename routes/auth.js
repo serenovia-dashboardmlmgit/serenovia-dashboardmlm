4323d2f3a2bc496a23fc5667bf144850febe8812
Mboute Blandine <blandinelvy2@gmail.com>
2026-06-14 12:04:15 +0100

    Fix import path for User model in auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../model/User.ts";
+import User from "../model/User.js";
 const router = Router();
 const JWT_SECRET = process.env.JWT_SECRET;
 router.post("/login", async (req, res) => {
